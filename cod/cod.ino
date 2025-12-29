4fb1868b65bea0b0d59eedfcdb3746a0fd2a9609
= <=>
2025-12-29 18:19:30 +0200

    cod initial cu eroare RFID

--- cod/cod.ino
+++ cod/cod.ino
@@ -3,31 +3,31 @@
 #include <Keypad.h>
 #include <SPI.h>
 #include <MFRC522.h>
-#include <Servo.h>
 
 
+/*  LCD  */
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-
+/*  RFID  */
 #define SS_PIN 10
 #define RST_PIN 9
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 
 
-Servo servo;
-const int servoPin = A1;
 
+bool seifDeschis = false;
 
+/*  INCUIETOARE  */
 const int lockPin = A2;
 
-
+/*  PIN  */
 String pin_corect = "1234";
 String pin_introdus = "";
 
-
+/* UID CORECT */
 byte uid_corect[] = {0xA6, 0x07, 0xA7, 0x00};
 
-
+/* ================= KEYPAD ================= */
 const byte ROWS = 4;
 const byte COLS = 4;
 
@@ -43,22 +43,58 @@ byte colPins[COLS] = {6, 7, 8, A0};
 
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
-/* FUNCTII */
+/*  FUNCTII  */
+
+void afisareInitiala() {
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Introdu PIN-ul");
+  lcd.setCursor(0,1);
+  lcd.print("sau card");
+}
+void resetLCD() {
+  lcd.clear();
+  delay(50);
+  lcd.init();
+  lcd.backlight();
+}
+
+void resetRFID() {
+  mfrc522.PCD_StopCrypto1();
+  mfrc522.PICC_HaltA();
+  delay(50);
+  mfrc522.PCD_Init();   
+}
 
 void deschideSeif() {
+  seifDeschis = true;
+
   lcd.clear();
   lcd.print("Acces permis");
 
-  digitalWrite(lockPin, HIGH); // activeaza incuietoarea
-  servo.write(90);             // deschide usa
+  digitalWrite(lockPin, HIGH);
 
-  delay(10000);                // 10 secunde deschis
 
-  servo.write(0);              // inchide usa
-  digitalWrite(lockPin, LOW);  // blocheaza
+  delay(5000);
+
+
+  digitalWrite(lockPin, LOW);
 
   lcd.clear();
+  lcd.print("Scoate cardul");
+
+  // Asteapta sa fie scos cardul
+  while (mfrc522.PICC_IsNewCardPresent()) {
+    delay(100);
+  }
+
+  resetRFID();
+  delay(500);
+
+  seifDeschis = false;
+  afisareInitiala();
 }
+
 
 bool verificaCardUID() {
   if (!mfrc522.PICC_IsNewCardPresent()) return false;
@@ -68,26 +104,29 @@ bool verificaCardUID() {
 
   for (byte i = 0; i < mfrc522.uid.size; i++) {
     if (mfrc522.uid.uidByte[i] != uid_corect[i]) {
+      resetRFID();
       return false;
     }
   }
 
-  mfrc522.PICC_HaltA();
+  resetRFID();
   return true;
 }
 
+/*  SETUP  */
 void setup() {
   Serial.begin(9600);
 
+  pinMode(SS_PIN, OUTPUT);
+  digitalWrite(SS_PIN, HIGH);
+
   lcd.init();
   lcd.backlight();
-  lcd.print("Introdu PIN-ul");
+  afisareInitiala();
 
   SPI.begin();
   mfrc522.PCD_Init();
 
-  servo.attach(servoPin);
-  servo.write(0);
 
   pinMode(lockPin, OUTPUT);
   digitalWrite(lockPin, LOW);
@@ -95,43 +134,39 @@ void setup() {
 
 void loop() {
 
-  /*KEYPAD*/
+  /*  KEYPAD  */
   char tasta = keypad.getKey();
 
   if (tasta) {
     if (tasta == '#') {
       if (pin_introdus == pin_corect) {
+        pin_introdus = "";
         deschideSeif();
       } else {
         lcd.clear();
         lcd.print("PIN gresit!");
-        delay(2000);
-        lcd.clear();
-        lcd.print("Introdu PIN-ul");
+        delay(1500);
+        pin_introdus = "";
+        afisareInitiala();
       }
-      pin_introdus = "";
     }
     else if (tasta == '*') {
       pin_introdus = "";
-      lcd.clear();
-      lcd.print("Reset PIN");
-      delay(1000);
-      lcd.clear();
-      lcd.print("Introdu PIN-ul");
+      afisareInitiala();
     }
     else {
       pin_introdus += tasta;
-      lcd.setCursor(0, 1);
-      lcd.print(pin_introdus);
+      lcd.setCursor(0,1);
+      lcd.print("                ");
+      lcd.setCursor(0,1);
+      for (byte i = 0; i < pin_introdus.length(); i++) {
+        lcd.print('*');
+      }
     }
   }
 
-  /*RFID*/
-  if (verificaCardUID()) {
-    lcd.clear();
-    lcd.print("Card valid");
-    delay(1000);
-    deschideSeif();
-    lcd.print("Introdu PIN-ul");
-  }
+  if (!seifDeschis && verificaCardUID()) {
+  deschideSeif();
+}
+
 }
